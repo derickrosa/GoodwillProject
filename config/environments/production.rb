40c9b83f2d17a0fb1393133dd0f7566e6725ca2c
derick <derick.edu@gmail.com>
2015-06-01 02:32:09 -0300

    database

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,7 +12,7 @@ Rails.application.configure do
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
-  config.action_controller.perform_caching = true
+  config.action_controller.perform_caching = true  
 
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
@@ -21,6 +21,9 @@ Rails.application.configure do
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_assets = true
+  # Heroku requires this to be false
+  config.assets.initialize_on_precompile=false
+  config.assets.precompile += ['static_pages.css']
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
